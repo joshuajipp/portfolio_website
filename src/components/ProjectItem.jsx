aa3d504700af990ec7decacc668189a574a7d88b
joshuajipp <joshuajipp@gmail.com>
2023-04-25 17:22:51 -0600

    grouped props

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-function ProjectItem({ image, name, id }) {
+function ProjectItem(props) {
   const navigate = useNavigate();
   return (
     <div
       className="projectItem"
       onClick={() => {
-        navigate("/project/" + id);
+        navigate("/project/" + props.id);
       }}
     >
-      <div style={{ backgroundImage: `url(${image})` }} className="bgImage" />
-      <h1> {name} </h1>
+      <div
+        style={{ backgroundImage: `url(${props.image})` }}
+        className="bgImage"
+      />
+      <h1> {props.name} </h1>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,19 @@ import GithubIcon from "@material-ui/icons/GitHub";
 import "../styles/Home.css";
 
 function Home() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <div className="about">
+        <h2> Hi, My Name is Joshua</h2>
+        <div className="prompt">
+          <p>Software Engineering Student at the University of Calgary.</p>
+          <LinkedInIcon />
+          <EmailIcon />
+          <GithubIcon />
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default Home;
