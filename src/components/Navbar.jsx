abf257811a5160c1f95e1a4c00c0371a53f6188b
joshuajipp <joshuajipp@gmail.com>
2023-04-25 15:04:25 -0600

    responsive navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,32 @@ import "../styles/Navbar.css";
 import ReorderIcon from "@material-ui/icons/Reorder";
 
 function Navbar() {
-  return <div className="navbar"></div>;
+  const [expandNavbar, setExpandNavbar] = useState(false);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
+
+  return (
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
+      <div className="toggleButton">
+        <button
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+        >
+          <ReorderIcon />
+        </button>
+      </div>
+      <div className="links">
+        <Link to="/"> Home </Link>
+        <Link to="/projects"> Projects </Link>
+        <Link to="/experience"> Experience </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
