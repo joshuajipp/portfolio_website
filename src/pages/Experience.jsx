9e86f30ed6be8410ddb3d49b8d66f4812d20613b
joshuajipp <joshuajipp@gmail.com>
2023-04-25 17:41:20 -0600

    added timeline react component

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,11 +1,44 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-import { ProjectList } from "../ProjectList";
-import GitHubIcon from "@material-ui/icons/GitHub";
-import "../styles/ProjectDisplay.css";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@material-ui/icons/School";
+import WorkIcon from "@material-ui/icons/Work";
 
-function ProjectDisplay() {
-  return <div className="project"></div>;
+function Experience() {
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date=""
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        ></VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date=""
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        ></VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date=""
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        ></VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date=""
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        ></VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
 
-export default ProjectDisplay;
+export default Experience;
