c2e80a58ccb64ee403e113d9782feeb41bff6c1c
joshuajipp <joshuajipp@gmail.com>
2023-04-27 18:15:44 -0600

    implemented legacy navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Experience from "./pages/Experience";
@@ -7,10 +8,20 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleClick = () => {
+    setIsDarkMode(!isDarkMode);
+    if (!isDarkMode) {
+      document.documentElement.classList.add("is-black");
+    } else {
+      document.documentElement.classList.remove("is-black");
+    }
+  };
   return (
     <div className="App">
       <Router>
-        <Navbar />
+        <Navbar handleClick={handleClick} isDarkMode={isDarkMode} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,32 +3,81 @@ import { Link, useLocation } from "react-router-dom";
 import "../styles/Navbar.css";
 import ReorderIcon from "@material-ui/icons/Reorder";
 
-function Navbar() {
-  const [expandNavbar, setExpandNavbar] = useState(false);
+function Navbar(props) {
+  const [isActive, setIsActive] = useState(false);
+  const isDesktop = window.innerWidth > 767;
 
-  const location = useLocation();
+  function handleMenuClick() {
+    setIsActive(!isActive);
+  }
+  function handleNavigationClick() {
+    setIsActive(false);
+  }
 
-  useEffect(() => {
-    setExpandNavbar(false);
-  }, [location]);
+  function handleDarkMode() {
+    props.handleClick();
+  }
 
   return (
-    <div className="navbar" id={expandNavbar ? "open" : "close"}>
-      <div className="toggleButton">
-        <button
-          onClick={() => {
-            setExpandNavbar((prev) => !prev);
-          }}
-        >
-          <ReorderIcon />
-        </button>
+    <header class="page__header header">
+      <div class="header__container container">
+        <div class="header__body">
+          <div class="header__leftside">
+            <Link to="/" title="" class="header__logo">
+              Joshua Jipp
+            </Link>
+          </div>
+          <div class="header__rightside">
+            <div
+              className={`header__menu header-menu ${isActive && "is-active"}`}
+            >
+              <ul class="header-menu__list">
+                <li class="header-menu__item">
+                  <Link
+                    to="/"
+                    className="header-menu__link"
+                    onClick={handleNavigationClick}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li class="header-menu__item">
+                  <Link
+                    to="/projects"
+                    class="header-menu__link"
+                    onClick={handleNavigationClick}
+                  >
+                    Projects
+                  </Link>
+                </li>
+                <li class="header-menu__item">
+                  <Link
+                    to="/experience"
+                    class="header-menu__link"
+                    onClick={handleNavigationClick}
+                  >
+                    Experience
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="moon-box" onClick={handleDarkMode}>
+              <i
+                className={`${
+                  props.isDarkMode ? "bx bx-sun" : "bx bx-moon"
+                } dm-icon`}
+              ></i>
+            </div>
+
+            <div className="header__mob" onClick={handleMenuClick}>
+              <div className="header__mob-menu">
+                <i className="bx bx-menu"></i>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="links">
-        <Link to="/"> Home </Link>
-        <Link to="/projects"> Projects </Link>
-        <Link to="/experience"> Experience </Link>
-      </div>
-    </div>
+    </header>
   );
 }
 
