3b5815c172fc933e0c08e5a702af80288c7e7c3d
Joshua Jipp <102313118+joshuajipp@users.noreply.github.com>
2023-09-10 12:31:16 -0600

    Update resume version

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -71,7 +71,7 @@ function Home(props) {
               </div>
               <div class="intro-soc__item">
                 <a
-                  href="https://drive.google.com/file/d/19y3dUKXRS1t9dX2imA4QlX-BbLedafjF/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1Q14aE5ZQDW00PolqM0lF47IyRwFrorWl/view?usp=sharing"
                   target="_blank"
                   rel="noopener noreferrer"
                   title="Resume"
